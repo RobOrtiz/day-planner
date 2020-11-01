1946e62cf80bbf9944b9358710fe11919c313c34
RobOrtiz <mrrobortiz@gmail.com>
2020-11-01 15:37:30 -0800

    update ja file and readme file

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-var moment = document.querySelector("#currentDay")
-moment().format('YYYY Do MM');
+$(document).ready(function() {
+    var test = false;
+// link current date from moment.js into jumbotron
+var now = moment().format('MMMM Do YYYY');
 
-var queryURL = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js";
+let nowHour24 = moment().format('H');
+let nowHour12 = moment().format('h');
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+if (test) {
+    nowHour24 = 13;
+    nowHour12 = 1;
+  }
+
+  let $dateHeading = $('#currentDay');
+  $dateHeading.text(now);
+
+// Enter task and save to planner
+let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+  // If plans were retrieved from localStorage, update the plan array to it
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    // this should only occur on first time the app is loaded in the browser
+    planTextArr = new Array(9);
+  }
+
+  // set variable referencing planner element
+  let $plannerDiv = $('#plannerContainer');
+  // clear existing elements
+  $plannerDiv.empty();
+
+  // build calendar by row for fix set of hours
+  for (let hour = 9; hour <= 22; hour++) {
+    // index for array use offset from hour
+    let index = hour - 9;
+
+  };
 });
-
-document.getElementById("save-button").addEventListener("click", function ()
-    {
-        var user = document.getElementById("user").value ;
-        //localStorage["user"] = user ;
-        localStorage.setItem("user", user);
-        console.log("")
-    } , false);
-
-    $(document).ready (function () {
-        $("*[save-button]").each(function () {
-          $(this).val(localStorage.getItem("item-" + $(this).attr("save-button")));
-        });
-      
-        $("*[save-button]").on("click", function (itm) {
-          localStorage.setItem ("item-" + $(this).attr("save-button"), $(this).val());
-        })
-      })
